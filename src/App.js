55d13345f99c30a78d4b334ceda11587c31e1219
iftekhs <iftekhstudio@gmail.com>
2022-12-25 15:02:18 +0600

    added Routes/Routes/Routes.js with / Route and addded Layouts/Main, Pages/Home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import routes from './Routes/Routes/Routes';
 
 function App() {
-  return <div className="App">
-    
-  </div>;
+  return <RouterProvider router={routes}></RouterProvider>;
 }
 
 export default App;
