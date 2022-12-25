2a1606f684eee2011c86d6b51883b50d260346e1
iftekhs <iftekhstudio@gmail.com>
2022-12-26 01:21:28 +0600

    added firebase config file

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyD_Ne86-NJgKgRv1hULlBfnz6zugNGOFO8',
+  authDomain: 'bookmeet-75a7d.firebaseapp.com',
+  projectId: 'bookmeet-75a7d',
+  storageBucket: 'bookmeet-75a7d.appspot.com',
+  messagingSenderId: '318899543465',
+  appId: '1:318899543465:web:98ed06e43642d2d59bf8b8',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
