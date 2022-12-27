9345e6b066711d071c85df73589c0e4b0e15d3db
iftekhs <iftekhstudio@gmail.com>
2022-12-28 02:54:31 +0600

    added the user and admin layout

---
+++ src/Layouts/Admin/Admin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Admin = () => {
+  return (
+    <div className="flex">
+      <aside></aside>
+      <main className="p-6"></main>
+    </div>
+  );
+};
+
+export default Admin;
---
+++ src/Layouts/User/User.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const User = () => {
+  return (
+    <div className="flex">
+      <aside></aside>
+      <main className="p-6"></main>
+    </div>
+  );
+};
+
+export default User;
